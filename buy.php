eea9818405d19a8b6ed205f610df59a1babc3237
Kashish <Kashish@Maheshs-MacBook-Air.local>
2019-06-19 15:32:24 +0530

    webpage to buy art pieces

---
+++ buy.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Art Gallary</title>
+    <link rel="stylesheet" href="master.css">
+  </head>
+  <body>
+    <form class="#" action="buy.html" method="post">
+      <h1 id="header">Art Gallary</h1>
+      <div class="login">
+        <a href="login.html"><button type="button" name="login_btn"><h3>Login</h3></button></a>
+      </div>
+      <div class="">
+
+      </div>
+    </form>
+    <footer>
+      <p>@Created By Kashish Gautam</p>
+    </footer>
+  </body>
+</html>
