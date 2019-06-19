88d03e9a72bd94393c03587674695cbdc8adbf07
Kashish <Kashish@Maheshs-MacBook-Air.local>
2019-06-19 15:30:46 +0530

    login Page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login page</title>
+    <link rel="stylesheet" href="master.css">
+  </head>
+  <body>
+    <h1 id="header">Art Gallary</h1>
+    <div class="login">
+      <a href="homepage.html"><button type="button" name="home_btn"><h3>Home</h3></button></a>
+    </div>
+    <div class="lgn_form">
+      <form class="form" action="login.html" method="post">
+        <h1>LOGIN</h1>
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" name="submit_btn" value="Login ">
+        <h5>Not a Registered user?<a href="registration.html"> Sign In</a></h5>
+      </form>
+    </div>
+    <footer>
+      <p>@Created By Kashish Gautam</p>
+    </footer>
+  </body>
+</html>
